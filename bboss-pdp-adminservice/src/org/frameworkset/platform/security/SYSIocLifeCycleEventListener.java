46f439767f172a406ae4bf5a97f3f7ad9611cde2
yinbp <yin-bp@163.com>
2017-03-16 10:50:24 +0800

    增加quartz和分布式事件的配置文件
    在ioc监听器中启动定时任务

--- bboss-pdp-adminservice/src/org/frameworkset/platform/security/SYSIocLifeCycleEventListener.java
+++ bboss-pdp-adminservice/src/org/frameworkset/platform/security/SYSIocLifeCycleEventListener.java
@@ -3,8 +3,10 @@ package org.frameworkset.platform.security;
 import java.util.Map;
 
 import org.frameworkset.platform.framework.Framework;
+import org.frameworkset.remote.EventUtils;
 import org.frameworkset.spi.BaseApplicationContext;
 import org.frameworkset.spi.event.IocLifeCycleEventListener;
+import org.frameworkset.task.TaskService;
 import org.frameworkset.web.servlet.context.WebApplicationContext;
 
 public class SYSIocLifeCycleEventListener implements IocLifeCycleEventListener {
@@ -21,6 +23,9 @@ public class SYSIocLifeCycleEventListener implements IocLifeCycleEventListener {
 		CacheManager.registRefreshEventListener();
 		//启动分布式事件服务
 		EventUtils.init();  */
+		TaskService service = TaskService.getTaskService();
+		service.startService();
+		EventUtils.init();
 		System.out.println("public void afterstart(BaseApplicationContext arg0) ");
 		Framework.getInstanceWithContext(((WebApplicationContext)arg0).getServletContext()).init();
 	}
